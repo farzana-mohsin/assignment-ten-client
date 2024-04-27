5b7f18e00525906913cf23ab9f9a1bd79662e6a4
Farzana Mohsin <fmohsin@usertesting.com>
2024-04-26 21:06:42 -0400

    test connection with vercel backend

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,4 +1,14 @@
+import { useEffect } from "react";
+
 const Home = () => {
+  useEffect(() => {
+    fetch("https://assignment-ten-server-lilac.vercel.app/items")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      });
+  });
+
   return (
     <div>
       <h2>this is home</h2>
