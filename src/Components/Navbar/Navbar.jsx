837d7cc10212942aae51d54515df9fb089e3db1a
Farzana Mohsin <fmohsin@usertesting.com>
2024-04-28 13:06:21 -0400

    implement light and dark theme

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { Link, NavLink } from "react-router-dom";
 import { AuthContext } from "../../Providers/AuthProvider";
 import { FaCrown } from "react-icons/fa";
@@ -7,6 +7,12 @@ import { FaCrown } from "react-icons/fa";
 
 const Navbar = () => {
   const { user, logOut } = useContext(AuthContext);
+  const [theme, setTheme] = useState("light");
+  const handleToggle = (e) => {
+    if (e.target.checked) {
+      setTheme("coffee");
+    } else setTheme("light");
+  };
 
   const navLinks = (
     <>
@@ -44,7 +50,7 @@ const Navbar = () => {
 
   return (
     <div>
-      <div className='navbar bg-black my-4 mx-auto '>
+      <div className='navbar md:px-10 py-7 bg-black mx-auto '>
         <div className='navbar-start'>
           <div className='dropdown'>
             <div
@@ -74,13 +80,13 @@ const Navbar = () => {
               {navLinks}
             </ul>
           </div>
-          <h1 className='animate__animated animate__fadeInLeft text-2xl md:text-3xl font-bold text-white pl-1'>
+          <h1 className='animate__animated animate__fadeInLeft text-xl md:text-3xl font-bold text-white pl-1'>
             Crown
           </h1>
           <div className='flex items-center justify-center text-4xl font-bold text-orange-400 ml-2'>
             <FaCrown />
           </div>
-          <h1 className='animate__animated animate__fadeInLeft text-2xl md:text-3xl font-bold text-white pl-1'>
+          <h1 className='animate__animated animate__fadeInLeft text-xl md:text-3xl font-bold text-white pl-1'>
             Art
           </h1>
         </div>
@@ -106,12 +112,16 @@ const Navbar = () => {
                 }
               /> */}
               <div
-                className='tooltip tooltip-bottom tooltip-accent rounded-full bg-transparent border-none text-white'
-                data-tip={user?.displayName || "user not found"}
+                className='tooltip tooltip-bottom border-none bg-none rounded-full mx-3'
+                data-tip={
+                  user?.displayName || (
+                    <button onClick={() => handleSignOut()}>Sign Out</button>
+                  )
+                }
               >
-                <button className='btn rounded-full bg-transparent border-none'>
+                <button className='rounded-full bg-none border-none w-12'>
                   <img
-                    className='w-10 md:block rounded-full bg-transparent border-none'
+                    className=' md:block rounded-full bg-none border-none'
                     alt='image not found'
                     src={
                       user?.photoURL ||
@@ -120,29 +130,63 @@ const Navbar = () => {
                   />
                 </button>
               </div>
-
-              <button
-                onClick={() => handleSignOut()}
-                className='btn bg-orange-400 text-white px-4 py-2 border text-sm rounded-xl ml-2'
-              >
-                Sign Out
-              </button>
             </div>
           ) : (
-            <>
+            <div className='flex flex-row'>
               <Link to='/login'>
-                <button className='btn  bg-orange-400 text-white px-4 py-2 border text-sm rounded-xl ml-2'>
+                <button className='btn  bg-orange-400 text-white md:px-4 md:py-2 border text-sm rounded-xl md:ml-2'>
                   Login
                 </button>
               </Link>
               <Link to='/register'>
-                <button className='btn  bg-orange-400 text-white px-4 py-2 border text-sm rounded-xl ml-2 '>
+                <button className='btn  bg-orange-400 text-white md:px-4 md:py-2 border text-sm rounded-xl ml-2 '>
                   Register
                 </button>
               </Link>
-            </>
+            </div>
           )}
         </div>
+        <label className='cursor-pointer grid place-items-center'>
+          <input
+            onChange={handleToggle}
+            type='checkbox'
+            value='coffee'
+            className='toggle theme-controller bg-base-content row-start-1 col-start-1 col-span-2'
+          />
+          <svg
+            className='col-start-1 row-start-1 stroke-base-100 fill-base-100'
+            xmlns='http://www.w3.org/2000/svg'
+            width='14'
+            height='14'
+            viewBox='0 0 24 24'
+            fill='none'
+            stroke='currentColor'
+            strokeWidth='2'
+            strokeLinecap='round'
+            strokeLinejoin='round'
+          >
+            <circle
+              cx='12'
+              cy='12'
+              r='5'
+            />
+            <path d='M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4' />
+          </svg>
+          <svg
+            className='col-start-2 row-start-1 stroke-base-100 fill-base-100'
+            xmlns='http://www.w3.org/2000/svg'
+            width='14'
+            height='14'
+            viewBox='0 0 24 24'
+            fill='none'
+            stroke='currentColor'
+            strokeWidth='2'
+            strokeLinecap='round'
+            strokeLinejoin='round'
+          >
+            <path d='M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z'></path>
+          </svg>
+        </label>
       </div>
     </div>
   );
